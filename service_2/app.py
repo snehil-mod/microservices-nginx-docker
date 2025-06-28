9733a4b5ccff8834b3966482fea6dcc23d94b696
snehil-mod <snehil.singh5151@gmail.com>
2025-06-28 15:56:57 +0530

    Update code and configuration after initial push

--- service_2/app.py
+++ service_2/app.py
@@ -12,6 +12,9 @@ def ping():
 def hello():
     return jsonify(message="Hello from Service 2")
 
+@app.route('/health')
+def health():
+    return jsonify({"status": "ok"}), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8002)
